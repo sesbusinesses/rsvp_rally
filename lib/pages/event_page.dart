aa378b2a7afa1088c9b88a4c70b55fdd94a92a1d
Ethan Dancho <ethan@dancho.org>
2024-05-20 17:36:59 -0700

    updated event pages

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rsvp_rally/widgets/eventcard.dart';
 import 'package:rsvp_rally/models/database_puller.dart';
+import 'package:semicircle_indicator/semicircle_indicator.dart';
 
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
@@ -37,8 +38,23 @@ class EventPageState extends State<EventPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children:
-                eventIds.map((eventId) => EventCard(eventID: eventId)).toList(),
+            children: [
+              const SemicircularIndicator(
+                color: Colors.orange,
+                bottomPadding: 0,
+                progress: 0.5,
+                child: Text(
+                  '50%',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              // Using the spread operator to insert all EventCard widgets into the children list
+              ...eventIds.map((eventId) => EventCard(eventID: eventId)).toList()
+            ],
           ),
         ),
       ),
