006c1def2c6d328f13403288cade8403a3e35b1b
Ethan Dancho <ethan@dancho.org>
2024-05-29 22:03:30 -0700

    Made poll_page.dart UI match event_page.dart UI. Allows the user to vote on a poll and see the results of the poll.

--- lib/pages/poll_page.dart
+++ lib/pages/poll_page.dart
@@ -4,39 +4,65 @@ import 'package:rsvp_rally/widgets/poll_card.dart';
 
 class PollPage extends StatelessWidget {
   final String eventID;
+  final String username;
 
-  const PollPage({super.key, required this.eventID});
+  const PollPage({super.key, required this.eventID, required this.username});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Event Poll'),
+        backgroundColor:
+            Colors.transparent, // Transparent background for AppBar
       ),
-      body: FutureBuilder<DocumentSnapshot>(
-        future:
-            FirebaseFirestore.instance.collection('Events').doc(eventID).get(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasData && snapshot.data != null) {
-              Map<String, dynamic> data =
-                  snapshot.data!.data() as Map<String, dynamic>;
-              Map<String, dynamic> polls =
-                  data['Polls'] as Map<String, dynamic>;
-              return ListView(
-                children: polls.entries.map((entry) {
-                  return PollCard(eventID: eventID, pollData: {
-                    'question': entry.key,
-                    'responses': entry.value
-                  });
-                }).toList(),
-              );
-            } else {
-              return const Text("No data available for this event.");
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFFfefdfd),
+              Color(0xFF5f42b2)
+            ], // White to purple gradient
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: FutureBuilder<DocumentSnapshot>(
+          future: FirebaseFirestore.instance
+              .collection('Events')
+              .doc(eventID)
+              .get(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData && snapshot.data != null) {
+                Map<String, dynamic> data =
+                    snapshot.data!.data() as Map<String, dynamic>;
+                Map<String, dynamic> polls =
+                    Map<String, dynamic>.from(data['Polls']);
+
+                return ListView(
+                  children: polls.entries.map((entry) {
+                    return PollCard(
+                      eventID: eventID,
+                      username: username,
+                      pollData: {
+                        'question': entry.key,
+                        'responses': entry.value
+                      },
+                    );
+                  }).toList(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                    child: Text("Error fetching data: ${snapshot.error}"));
+              } else {
+                return const Center(
+                    child: Text("No data available for this event."));
+              }
             }
-          }
-          return const CircularProgressIndicator();
-        },
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
     );
   }
