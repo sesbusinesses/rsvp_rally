5900a757edc6fa4be4fa8ebb3b7089a87fabc965
Ethan Dancho <ethan@dancho.org>
2024-07-04 00:20:12 -0700

    now heic can be used as the EventImageDisplay

--- lib/widgets/event_image_display.dart
+++ lib/widgets/event_image_display.dart
@@ -1,19 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as img;
 import 'dart:io';
 import 'dart:convert';
+import 'dart:math' as math;
 
 class EventImageDisplay extends StatefulWidget {
   final String eventID;
   final double rating;
   final bool clickable;
 
-  const EventImageDisplay(
-      {super.key,
-      required this.eventID,
-      required this.rating,
-      required this.clickable});
+  const EventImageDisplay({
+    super.key,
+    required this.eventID,
+    required this.rating,
+    required this.clickable,
+  });
 
   @override
   _EventImageDisplayState createState() => _EventImageDisplayState();
@@ -51,10 +54,27 @@ class _EventImageDisplayState extends State<EventImageDisplay> {
       final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
       if (image != null) {
         File file = File(image.path);
-        String base64Image = base64Encode(await file.readAsBytes());
+        List<int> imageBytes = await file.readAsBytes();
+
+        // Resize the image if it is too large
+        if (imageBytes.length > 1000000) {
+          // Example threshold: 1MB
+          img.Image? originalImage = img.decodeImage(imageBytes);
+          if (originalImage != null) {
+            // Calculate the reduction factor to keep the size under 1MB
+            double reductionFactor = math.sqrt(10000 / imageBytes.length);
+            img.Image resizedImage = img.copyResize(originalImage,
+                width: (originalImage.width * reductionFactor).toInt());
+            imageBytes = img.encodeJpg(resizedImage);
+          }
+        }
+
+        String base64Image = base64Encode(imageBytes);
         setState(() {
           _imageBase64 = base64Image;
         });
+        print(base64Image);
+        print(base64Image.length);
         await FirebaseFirestore.instance
             .collection('Events')
             .doc(widget.eventID)
