720aafbefb1345f86fcc3112f4e6b9b80ec9d616
Ethan Dancho <ethan@dancho.org>
2024-07-15 15:43:04 -0700

    addFriend button dissapears after adding friend

--- lib/widgets/user_card.dart
+++ lib/widgets/user_card.dart
@@ -280,6 +280,9 @@ class _UserCardState extends State<UserCard> {
                             buttonText: "Add Friend",
                             onPressed: () {
                               addFriend(widget.username);
+                              setState(() {
+                                isFriend = true;
+                              });
                             },
                             rating: rating,
                             smallVersion: true,
