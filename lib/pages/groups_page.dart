551ccaa729fa5fd0f6c90e864bb7f25d1820a0f5
Ethan Dancho <ethan@dancho.org>
2024-07-22 13:19:20 -0700

    We can now create groups

--- lib/pages/groups_page.dart
+++ lib/pages/groups_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rsvp_rally/models/colors.dart';
 import 'package:rsvp_rally/widgets/group_card.dart';
+import 'package:rsvp_rally/pages/group_create_page.dart';
 
 class GroupsPage extends StatelessWidget {
   final String username;
@@ -14,41 +16,57 @@ class GroupsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: FutureBuilder<DocumentSnapshot>(
-        future:
-            FirebaseFirestore.instance.collection('Users').doc(username).get(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-                child: CupertinoActivityIndicator(
-              radius: 15,
-            ));
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || !snapshot.data!.exists) {
-            return const Center(child: Text('No groups found'));
-          } else {
-            var userData = snapshot.data!;
-            var groupIDs = List<String>.from(userData['Groups']);
+        body: FutureBuilder<DocumentSnapshot>(
+          future: FirebaseFirestore.instance
+              .collection('Users')
+              .doc(username)
+              .get(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                  child: CupertinoActivityIndicator(
+                radius: 15,
+              ));
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || !snapshot.data!.exists) {
+              return const Center(child: Text('No groups found'));
+            } else {
+              var userData = snapshot.data!;
+              var groupIDs = List<String>.from(userData['Groups']);
 
-            return Padding(
-                padding: EdgeInsets.only(
-                    top: 20,
-                    left: screenSize.width * 0.075,
-                    right: screenSize.width * 0.075),
-                child: ListView.builder(
-                  itemCount: groupIDs.length,
-                  itemBuilder: (context, index) {
-                    return GroupCard(
-                      groupID: groupIDs[index],
-                      userRating: userRating,
-                      username: username,
-                    );
-                  },
-                ));
-          }
-        },
-      ),
-    );
+              return Padding(
+                  padding: EdgeInsets.only(
+                      top: 20,
+                      left: screenSize.width * 0.075,
+                      right: screenSize.width * 0.075),
+                  child: ListView.builder(
+                    itemCount: groupIDs.length,
+                    itemBuilder: (context, index) {
+                      return GroupCard(
+                        groupID: groupIDs[index],
+                        userRating: userRating,
+                        username: username,
+                      );
+                    },
+                  ));
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          shape: const CircleBorder(),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => GroupCreatePage(
+                        username: username,
+                        rating: userRating,
+                      )),
+            );
+          },
+          backgroundColor: getInterpolatedColor(userRating),
+          child: const Icon(Icons.add, color: AppColors.light),
+        ));
   }
 }
